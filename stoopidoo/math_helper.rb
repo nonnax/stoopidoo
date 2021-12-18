8154d5d253a8ee48c2f7e8ad7804c6258a2b0010
nonnax <ironald@gmail.com>
2021-12-18 15:58:53 +0800

    update: math_helper.rb

--- stoopidoo/math_helper.rb
+++ stoopidoo/math_helper.rb
@@ -4,6 +4,6 @@ def shuffle!(choices, **h)
 	# random shuffling using tail n-digits of msec
   n = h[:factor] ||= 2
   n *= -1
-  reps=rand(Time.now.to_ms.to_s[n..-1].to_i)
+  reps=rand(Time.now.to_ms.to_s[n..-1].to_i).to_i
   reps.times { choices.shuffle! }
 end
